bfc9793934af902b7be2dc1d10fea39f817bc0dc
Youssouf Sergma <sil3nthilldz@gmail.com>
2023-09-12 00:55:30 +0100

    completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home" style={{color:'firebrick'}}><h1>Your {name} is a Web Developer from Your {city}</h1></div>;
 }
 
 export default Home;
